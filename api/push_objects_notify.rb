3d8c1dcfd9dcf9d954931dcd508d088f547932a0
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-02 08:26:51 -0600

    debugging deploy issue

--- api/push_objects_notify.rb
+++ api/push_objects_notify.rb
@@ -4,15 +4,15 @@ Rhosync::Server.api :push_objects_notify do |params,user|
   source_sync = SourceSync.new(source)
   source_sync.push_objects(params[:objects])
   
-  begin
-    puts "push_objects_notify called, notifying observer for #{params[:source_id]}"
-    klass = Object.const_get(params[:source_id])
-    if klass
-      klass.api_pushed(params[:user_id]) if klass.respond_to?(:api_pushed)
-    else
-      puts "Unable to create class object for #{params[:source_id]}"
-    end
-  rescue Exception => e
-    puts e.inspect
-  end
+  # begin
+  #   puts "push_objects_notify called, notifying observer for #{params[:source_id]}"
+  #   klass = Object.const_get(params[:source_id])
+  #   if klass
+  #     klass.api_pushed(params[:user_id]) if klass.respond_to?(:api_pushed)
+  #   else
+  #     puts "Unable to create class object for #{params[:source_id]}"
+  #   end
+  # rescue Exception => e
+  #   puts e.inspect
+  # end
 end
