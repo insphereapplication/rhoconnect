e33714a211f79cf9c12911c22449c115cd358cfa
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-02 08:31:58 -0600

    debugging deploy issue

--- api/push_objects_notify.rb
+++ api/push_objects_notify.rb
@@ -1,18 +1,18 @@
 
-Rhosync::Server.api :push_objects_notify do |params,user|
-  source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
-  source_sync = SourceSync.new(source)
-  source_sync.push_objects(params[:objects])
-  
-  # begin
-  #   puts "push_objects_notify called, notifying observer for #{params[:source_id]}"
-  #   klass = Object.const_get(params[:source_id])
-  #   if klass
-  #     klass.api_pushed(params[:user_id]) if klass.respond_to?(:api_pushed)
-  #   else
-  #     puts "Unable to create class object for #{params[:source_id]}"
-  #   end
-  # rescue Exception => e
-  #   puts e.inspect
-  # end
-end
+# Rhosync::Server.api :push_objects_notify do |params,user|
+#   source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
+#   source_sync = SourceSync.new(source)
+#   source_sync.push_objects(params[:objects])
+#   
+#   begin
+#     puts "push_objects_notify called, notifying observer for #{params[:source_id]}"
+#     klass = Object.const_get(params[:source_id])
+#     if klass
+#       klass.api_pushed(params[:user_id]) if klass.respond_to?(:api_pushed)
+#     else
+#       puts "Unable to create class object for #{params[:source_id]}"
+#     end
+#   rescue Exception => e
+#     puts e.inspect
+#   end
+# end
--- sources/contact.rb
+++ sources/contact.rb
@@ -1,15 +1,15 @@
 class Contact < SourceAdapter
   
-  on_api_push do |user_id|
-    puts "Pinging #{user_id} from SourceAdapter callback..."
-    PingJob.perform(
-      'user_id' => user_id,
-      'sources' => ['Contact'],
-      'message' => 'Pinged, thusly',
-      'vibrate' => 2000,
-      'sound' => 'hello.mp3'
-    )
-  end
+  # on_api_push do |user_id|
+  #   puts "Pinging #{user_id} from SourceAdapter callback..."
+  #   PingJob.perform(
+  #     'user_id' => user_id,
+  #     'sources' => ['Contact'],
+  #     'message' => 'Pinged, thusly',
+  #     'vibrate' => 2000,
+  #     'sound' => 'hello.mp3'
+  #   )
+  # end
   
   def initialize(source,credential)
     @contact_url = "http://75.31.122.27/contact"
