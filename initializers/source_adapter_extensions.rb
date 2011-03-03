81d51d5be903787f92e52e52227940d2548f91ac
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-03 14:26:18 -0600

    try using an instance var of the class to store the notify block.

--- initializers/source_adapter_extensions.rb
+++ initializers/source_adapter_extensions.rb
@@ -1,11 +1,11 @@
 module Rhosync
   class SourceAdapter
     def self.on_api_push(&block)
-      @@api_push_observer = block
+      @api_push_observer = block
     end
     
     def self.notify_api_pushed(user_id)
-      @@api_push_observer.call(user_id) if @@api_push_observer
+      @api_push_observer.call(user_id) if @api_push_observer
     end
   end
 end
