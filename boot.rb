b6132ba3a5f4cda991ef71ebf71e619ac5fdc314
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-17 14:21:16 -0500

    try to set redis with a new redis object

--- boot.rb
+++ boot.rb
@@ -14,7 +14,9 @@ require 'ap'
    
 CONFIG = YAML::load_file('settings/config.yml')
 
-Resque.redis = "redis://filefish.redistogo.com:9501"
+uri = URI.parse("redis://filefish.redistogo.com:9501")
+
+Resque.redis = Redis.new(:host => uri.host, :port => uri.port)
 Resque.schedule = YAML.load_file(File.join(File.dirname(__FILE__), 'settings/resque_schedule.yml'))
 Resque::Scheduler.run if fork.nil?
 Resque::Worker.new('clean_old_opportunity_data').work(1) if fork.nil? 
