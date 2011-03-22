b43584a8a5ab6729b6bfc6ce2581a899af0105cf
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-22 17:01:12 -0500

    needed to map regardingobjectid values coming from CRM for the push API

--- api/push_objects_notify.rb
+++ api/push_objects_notify.rb
@@ -4,7 +4,16 @@ Rhosync::Server.api :push_objects_notify do |params,user|
   
   source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
   source_sync = SourceSync.new(source)
-  source_sync.push_objects(params[:objects])
+  
+  if params[:source_id] == 'Activity'
+    ap "PROCESSING ACTIVITIES..."
+    objects = ActivityMapper.map_json(params[:objects])
+    ap objects
+  else
+    objects = params[:objects]
+  end
+  
+  source_sync.push_objects(mapped_hash)
   
   begin
     ap "push_objects_notify called, notifying observer for #{params[:source_id]}"
@@ -15,3 +24,15 @@ Rhosync::Server.api :push_objects_notify do |params,user|
     log e.inspect
   end
 end
+
+
+# {
+#   "api_token" => "6f609e8a5c1c4baa96c7d520375ab7a6",
+#   "user_id" => "dhamo.raj",
+#        "objects" => {
+#          "b9930e04-f94b-e011-93bf-0050569c7cfe" => {
+#              "cssi_lastactivitydate" => "03/22/2011 04:49:36 PM"
+#          }
+#      },
+#      "source_id" => "Opportunity"
+#  }
--- mappers/activity_mapper.rb
+++ mappers/activity_mapper.rb
@@ -12,10 +12,32 @@ class ActivityMapper < Mapper
   end 
   
   # {
+  #   "api_token" => "6f609e8a5c1c4baa96c7d520375ab7a6",
+  #   "user_id" => "dhamo.raj",
+  #        "objects" => {
+  #          "b9930e04-f94b-e011-93bf-0050569c7cfe" => {
+  #                "cssi_lastactivitydate" => "03/22/2011 04:49:36 PM"
+  #                "regardingobjectid" => "{
+  #                   'type' => "asdf"
+  #                   'id' => '123'
+  #               }
+  #          }
+  #      },
+  #      "source_id" => "Opportunity"
+  #  }
+  
+  # {
   #       "parent_type" => "Opportunity",
   #              "type" => "PhoneCall",
   #         "parent_id" => "840af314-b246-e011-93bf-0050569c7cfe"
   # }
+  
+  # {
+  #         "statecode" => "Completed",
+  #                "id" => "89d67d58-9454-e011-93bf-0050569c7cfe",
+  #      "scheduledend" => "2011-03-22 10:07:34 -0500"
+  #  }
+  
   
    # {
    #       "regardingobjectid" => "{
