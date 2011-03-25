9226acff491c68beda3c98ea622c279a1b040657
Brock Spratlen <brockspratlen@gmail.com>
2011-03-25 13:35:38 -0500

    Added functionality that automatically notifies the proxy of orphaned tokens by calling the "logout" action. Prior to this change a single user would generate dozens of authentication requests per hour, of which only one is being used by RhoSync for that user. This was not sustainable.

--- application.rb
+++ application.rb
@@ -8,12 +8,27 @@ class Application < Rhosync::Base
       ap "Logging onto #{CONFIG[:crm_path]}session/logon"
       response = RestClient.post "#{CONFIG[:crm_path]}session/logon", :username => username, :password => password
       ap response.to_s
+      success = false
       if response.code == 200
-        Store.put_value("username:#{username.downcase}:token", response.body.strip.gsub(/"/, ''))
-        return true
-      elsif response.code == 401
-        return false
-      end   
+        new_token = response.body.strip.gsub(/"/, '')
+        
+        begin
+          #get old token, logout from proxy if it exists
+          old_token = Store.get_value("username:#{username.downcase}:token")
+          if old_token
+            ap "Found old token #{old_token}, logging it out from proxy"
+            logout_response = RestClient.post "#{CONFIG[:crm_path]}session/logout", {:token => old_token}
+            ap "Logout response code #{logout_response.code}"
+          end
+        rescue
+          ap "Error while removing old token"
+        end
+        
+        Store.put_value("username:#{username.downcase}:token", new_token)
+        success = true
+      end
+      
+      return success 
     end
     
     def initializer(path)
