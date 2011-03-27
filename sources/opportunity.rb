3a6b07bde88d4382c737537b530baabc006f07a4
Dave Sims <dave.sims@parivedasolutions.com>
2011-03-26 19:23:28 -0500

    don't ping with sources argument

--- sources/opportunity.rb
+++ sources/opportunity.rb
@@ -5,7 +5,6 @@ class Opportunity < SourceAdapter
   on_api_push do |user_id|
      PingJob.perform(
        'user_id' => user_id,
-       'sources' => ['Contact', 'Activity', 'Note', 'Opportunity'],
        'message' => 'You have new Opportunities',
        'vibrate' => '2000',
        'sound' => 'hello.mp3'
