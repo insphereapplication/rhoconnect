620a5aed8ab19172282344f3a02105db4b4cb4ca
Brock Spratlen <brockspratlen@gmail.com>
2011-03-30 00:53:46 -0500

    Removing logging from push api methods; Heroku logs were unusable

--- api/push_mapped_objects.rb
+++ api/push_mapped_objects.rb
@@ -1,14 +1,14 @@
 Rhosync::Server.api :push_mapped_objects do |params,user|
-  ap "PUSH MAPPED #{params[:source_id]} OBJECTS FOR #{user}"
-  ap params
+  # ap "PUSH MAPPED #{params[:source_id]} OBJECTS FOR #{user}"
+  #   ap params
   
   source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
   source_sync = SourceSync.new(source)
   
   objects = Mapper.map_source_data(params[:objects], params[:source_id])
   
-  ap "PARSED OBJECTS:"
-  ap objects
+  # ap "PARSED OBJECTS:"
+  #   ap objects
   
   source_sync.push_objects(objects)
 
