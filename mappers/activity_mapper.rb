559b728781e2a64a5bcd82b1b3357ca989c65efa
Brock Spratlen <brockspratlen@gmail.com>
2011-03-30 00:48:29 -0500

    Adding diagnostics to activity mapper

--- mappers/activity_mapper.rb
+++ mappers/activity_mapper.rb
@@ -1,6 +1,8 @@
 
 class ActivityMapper < Mapper
   def map_from_source_hash(activity_array)
+    puts "Activity array before"
+    ap activity_array
     activity_array.map! do |value| 
       parentprops = value['regardingobjectid'] 
       unless parentprops.blank?
@@ -8,6 +10,8 @@ class ActivityMapper < Mapper
         value.merge!({'parent_id' => parentprops['id'], 'parent_type' => parentprops['type']})
       end
     end
+    puts "Activity array after"
+    ap activity_array
     activity_array.reduce({}){|sum, value| sum[value["activityid"]] = value if value; sum }
   end 
   
