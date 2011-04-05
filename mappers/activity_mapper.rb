dd9f9b209aa4266d47cbdddb0d720b4868c715f6
Brock Spratlen <brockspratlen@gmail.com>
2011-04-05 15:21:25 -0500

    Fixed bug in mapper where blank properties weren't being rejected as expected and Rhodes was rejecting the entire entity because it couldn't map these proxy<->RhoSync-only fields to a fixed schema field on the client.

--- mappers/activity_mapper.rb
+++ mappers/activity_mapper.rb
@@ -3,20 +3,21 @@ class ActivityMapper < Mapper
   def map_from_source_hash(activity_array)
     activity_array.map! do |value| 
       parentprops = value['regardingobjectid'] 
-      unless parentprops.blank?
+      unless parentprops.nil?
         value.reject!{|k,v| k == 'regardingobjectid'}
-        value.merge!({'parent_id' => parentprops['id'], 'parent_type' => Mapper.convert_type_name(parentprops['type'])})
+        value.merge!({'parent_id' => parentprops['id'], 'parent_type' => Mapper.convert_type_name(parentprops['type'])}) unless parentprops.blank?
       end
       
       recipient_field_name = ActivityMapper.get_recipient_field_name(value['type'])
-      unless value[recipient_field_name].blank?
-        value.merge!({'parent_contact_id' => value[recipient_field_name][0]['id']})
+      recipient_value = value[recipient_field_name]
+      unless recipient_value.nil?
+        value.merge!({'parent_contact_id' => recipient_value[0]['id']}) unless recipient_value.blank?
         value.reject!{|k,v| k == recipient_field_name}
       end
       
       #always filter out attributes that are only set in RhoSync (avoids problems with fixed schema)
-      #never should be modified from rhodes and should only be injected in map_data_from_client as needed
-      value.reject!{|k,v|  ['cssi_skipdispositionworkflow','organizer','from'].include?(k) }
+      #these fields are not modified from rhodes and should only be injected in map_data_from_client as needed
+      value.reject!{|k,v|  ['cssi_skipdispositionworkflow','organizer','from','cssi_fromrhosync'].include?(k) }
       value
     end
     activity_array.reduce({}){|sum, value| sum[value["activityid"]] = value if value; sum }
@@ -46,6 +47,7 @@ class ActivityMapper < Mapper
         }) unless data['parent_contact_id'].blank?
       data.reject!{|k,v| k == 'parent_contact_id'}
     end
+    
     data
   end
   
