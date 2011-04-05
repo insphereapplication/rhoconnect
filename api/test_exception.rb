c7d1cd5efb44c3cd30f9fc012cb6bd4954eebf28
Dave Sims <dave.sims@parivedasolutions.com>
2011-04-05 18:04:05 -0500

    test with Exceptional handler

--- api/test_exception.rb
+++ api/test_exception.rb
@@ -1,7 +1,11 @@
+set :raise_errors, true
+use Rack::Exceptional, 'b8788d7b2ae404c9661f40215f5d9258aede9c83'
+
 Rhosync::Server.api :test_exception do |params,user|
   puts "Raising test exception..."
   
-  raise "Raised a test exception from API. Boom."
-
+  Exceptional.rescue do
+    raise "Raised a test exception from API. Boom."
+  end
   
 end
