ed93be8e1fc1756b437cba2b36e73462796eac95
Dave Sims <dave.sims@parivedasolutions.com>
2011-04-05 15:49:32 -0500

    exceptional testing

--- api/test_exception.rb
+++ api/test_exception.rb
@@ -1,8 +1,7 @@
 Rhosync::Server.api :test_exception do |params,user|
   puts "Raising test exception..."
   
-  Exceptional.rescue do
-    raise "Raised a test exception from API. Boom."
-  end
+  raise "Raised a test exception from API. Boom."
+
   
 end
