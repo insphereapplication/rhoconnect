9b38b9e6a31d9a48bbe3d6fd53dfed96a8c5ce07
Dave Sims <davsims@gmail.com>
2011-05-09 22:18:59 -0500

    tweaks to log/settings etc.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :application, "InsiteMobile"
 set :domain,      "mobile.insphere.com"
 set :repository,  "git@git.rhohub.com:insphere/InsiteMobile-dev-rhosync.git"
-set :branch, "onsite_master"
+set :branch,      "onsite_master"
 set :use_sudo,    false
 set :deploy_to,   "/var/www/#{application}"
 set :deploy_via, :copy
@@ -12,15 +12,15 @@ server "10.143.28.181", :app, :web
 
 namespace :deploy do
   task :start, :roles => :app do
-    run "apachectl restart"
+    run "/usr/sbin/apachectl restart"
   end
 
   task :stop, :roles => :app do
-    run  "apachectl stop"
+    run  "/usr/sbin/apachectl stop"
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-     run "apachectl restart"
+     run "/usr/sbin/apachectl restart"
   end
 end
