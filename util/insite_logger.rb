b9e15e92794d77d4ba38f2b0a7657c3c36f46a0e
Dave Sims <davsims@gmail.com>
2011-05-10 10:31:26 -0500

    tweaks and documentation for settings.yml

--- util/insite_logger.rb
+++ util/insite_logger.rb
@@ -16,7 +16,7 @@ module InsiteLogger
     
     if log_conf[:mode] == 'file' 
       Dir.mkdir(File.dirname(log_conf[:path])) unless File.exists?(File.dirname(log_conf[:path]))
-      @logger = Logger.new(log_conf[:path], log_conf[:num_old_logs], log_conf[:max_log_size]) 
+      @logger = Logger.new(log_conf[:path], log_conf[:num_archived_logs], log_conf[:max_log_size]) 
     end
   end
 end
