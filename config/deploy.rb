9d0e70f37059a6c0e49e85c34eba8f5b4a4bf75b
Dave Sims <davsims@gmail.com>
2011-05-10 14:46:00 -0500

    moving to nrhrho101/103 from ip in deploy and settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,26 +1,27 @@
 set :application, "InsiteMobile"
 set :domain,      "mobile.insphere.com"
 set :repository,  "git@git.rhohub.com:insphere/InsiteMobile-dev-rhosync.git"
-set :branch, "onsite_master"
+set :branch,      "onsite_master"
 set :use_sudo,    false
 set :deploy_to,   "/var/www/#{application}"
 set :deploy_via, :copy
 set :scm,         :git
 set :user,        "cap"
+set :normalize_asset_timestamps, false
 
-server "10.143.28.181", :app, :web
+role :app, "nrhrho101"
 
 namespace :deploy do
   task :start, :roles => :app do
-    run "apachectl restart"
+    run "/usr/sbin/apachectl restart"
   end
 
   task :stop, :roles => :app do
-    run  "apachectl stop"
+    run  "/usr/sbin/apachectl stop"
   end
 
   desc "Restart Application"
   task :restart, :roles => :app do
-     run "apachectl restart"
+     run "/usr/sbin/apachectl restart"
   end
 end
