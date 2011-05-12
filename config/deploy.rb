964c4b37e129acebb3415446ced70abc610e7bab
Dave Sims <davsims@gmail.com>
2011-05-12 14:17:33 -0500

    comment for license key deploy task

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,5 +30,9 @@ end
 
 desc "Set the Rhosync license key for the particular host machine"
 task :set_license , :roles => :app do
+  # this task assumes that there is a license key file named "<hostname>_license.key" in the settings directory
+  # in source control for every deployment target defined above in "role :app, '<hostname>', '<hostname>'", etc.
+  # It will copy the server-specific license key to the /settings/license.key file which Rhosync will use
+  # for that server.
   run "mv #{current_release}/settings/$CAPISTRANO:HOST$_license.key #{current_release}/settings/license.key"
 end
