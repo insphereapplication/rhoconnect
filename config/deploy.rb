61c39a6ff8d589345feb31fd80329f30007b0daf
Dave Sims <davsims@gmail.com>
2011-05-17 18:37:10 -0500

    set passenger processes to 10

--- config/deploy.rb
+++ config/deploy.rb
@@ -51,8 +51,8 @@ namespace :deploy do
     passenger_module = "/opt/ruby-enterprise-1.8.7-2011.03/lib/ruby/gems/1.8/gems/passenger-3.0.7/ext/apache2/mod_passenger.so"
     passenger_root = "/opt/ruby-enterprise-1.8.7-2011.03/lib/ruby/gems/1.8/gems/passenger-3.0.7"
     passenger_pool_idle_time = 0
-    max_rhosync_processes = 3
-    min_rhosync_processes = 3
+    max_rhosync_processes = 10
+    min_rhosync_processes = 10
     ruby_bin = "/opt/ruby-enterprise-1.8.7-2011.03/bin/ruby"
     current_release = "/var/www/InsiteMobile/current"
     server_name = "https://rhosync.insphereis.net"
