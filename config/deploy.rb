6ef2e9cf42c908a161143abf9e19df8134b792f3
Dave Sims <davsims@gmail.com>
2011-05-19 13:55:45 -0500

    tweaks to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,8 +15,8 @@ set :env, :onsite
 # apache/passenger config properties -- these are used by templates/httpd.conf.erb
 set :document_root, "#{deploy_to}/current/public"
 set :server_name, "https://rhosync.insphereis.net"
-set :max_rhosync_processes, 2
-set :min_rhosync_processes, 2
+set :max_rhosync_processes, 3
+set :min_rhosync_processes, 3
 set :passenger_pool_idle_time, 0
 set :passenger_log_level, 3
 set :web_port, "80"
