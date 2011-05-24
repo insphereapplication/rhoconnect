c0ecd633fde02f79a52e4d76aa547281a744431b
Dave Sims <davsims@gmail.com>
2011-05-24 15:49:54 -0500

    tweak to logger hostname output

--- util/insite_logger.rb
+++ util/insite_logger.rb
@@ -1,10 +1,10 @@
 module InsiteLogger  
 
   def self.info(message)
-    # output_host_name
+    output_host_name
     ap message  
     message = message.kind_of?(Array) ? message.join("\n") : message.inspect
-    insite_logger.info(message) if insite_logger
+    insite_logger.info("#{host_name} -- #{message}") if insite_logger
   end
   
   def self.insite_logger
@@ -26,8 +26,9 @@ module InsiteLogger
   end
   
   def self.host_name
-    @host_name ||= `echo $HOSTNAME`.strip
-  rescue
-    ""
+      @host_name ||= `echo $HOSTNAME`.strip
+    rescue
+      ""
+    end
   end
 end
