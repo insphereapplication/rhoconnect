7ca8a5276862004d3b8b740ad953d12705706023
Dave Sims <davsims@gmail.com>
2011-05-25 19:21:50 -0500

    add min passenger instances to httpd.config

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,6 +23,7 @@ set :time, Time.now.strftime('%m/%d/%Y %r')
 # passenger config properties -- see: templates/httpd.conf.erb
 set :passenger_pool_idle_time, 0
 set :passenger_max_pool_size, 30
+set :passenger_min_instances, 10
 set :passenger_log_level, 3
 set :passenger_module, "/opt/ruby-enterprise-1.8.7-2011.03/lib/ruby/gems/1.8/gems/passenger-3.0.7/ext/apache2/mod_passenger.so"
 set :passenger_root, "/opt/ruby-enterprise-1.8.7-2011.03/lib/ruby/gems/1.8/gems/passenger-3.0.7"
