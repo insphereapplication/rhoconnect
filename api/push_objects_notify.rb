875b586ab382a6942e9569e535afe9337c8725be
Dave Sims <davsims@gmail.com>
2011-05-26 10:14:36 -0500

    tweak to debug output

--- api/push_objects_notify.rb
+++ api/push_objects_notify.rb
@@ -1,10 +1,7 @@
 Rhosync::Server.api :push_objects_notify do |params,user|
   ExceptionUtil.rescue_and_reraise do
-    InsiteLogger.info "PUSH OBJECTS NOTIFY #{params[:source_id]} OBJECTS FOR #{user.inspect}"
-    InsiteLogger.info params[:source_id].inspect
-    InsiteLogger.info params['source_id'].inspect
-    InsiteLogger.info params[:user_id].inspect
-    InsiteLogger.info params['user_id'].inspect
+    InsiteLogger.info "PUSH OBJECTS NOTIFY #{params[:source_id]} OBJECTS FOR #{params[:user_id]}"
+    InsiteLogger.info caller
     InsiteLogger.info "#"*80
 
     source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
