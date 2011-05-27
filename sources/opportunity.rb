d7c1164165ad49f1b973a704df447ca9b0f43ea2
Kyle Norton <kyle.norton@parivedasolutions.com>
2011-05-27 16:09:47 -0500

    reducing vibrate to 2

--- sources/opportunity.rb
+++ sources/opportunity.rb
@@ -8,7 +8,7 @@ class Opportunity < SourceAdapter
        PingJob.perform(
          'user_id' => user_id,
          'message' => 'You have new Opportunities',
-         'vibrate' => '500',
+         'vibrate' => '2',
          'sound' => 'hello.mp3'
        )
      end
