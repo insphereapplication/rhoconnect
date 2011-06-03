8ffacbaffca346877d6507356bcd05f6989e6501
Greg Norz <greg.norz@parivedasolutions.com>
2011-06-03 16:09:31 -0500

    Adding in new StaticEntity source adapter to hand back selected picklist arrays from CRM.

---
+++ sources/static_entity.rb
@@ -0,0 +1,92 @@
+class StaticEntity < SourceAdapter
+  def initialize(source,credential)
+    InsiteLogger.info "******************** Calling initialize from StaticEntity ********************"
+    ExceptionUtil.rescue_and_reraise do
+      @staticentity_url = "#{CONFIG[:crm_path]}staticentity"
+      super(source,credential)
+    end
+  end
+ 
+  def login
+    InsiteLogger.info "******************** Calling login from StaticEntity ********************"
+    ExceptionUtil.rescue_and_reraise do
+      @username = Store.get_value("username:#{current_user.login.downcase}:username")
+      
+      encryptedPassword = Store.get_value("username:#{current_user.login.downcase}:password")
+      @password = Crypto.decrypt( encryptedPassword )
+      
+      @initialized_key = "username:#{current_user.login.downcase}:dependent:initialized"
+    end
+  end
+ 
+  def query(params=nil)
+    ExceptionUtil.rescue_and_reraise do
+      InsiteLogger.info "QUERY FOR STATICENTITIES for #{current_user.login.downcase}"
+      
+      carrier_res = JSON.parse(RestClient.post(@staticentity_url, {:username => @username,
+                                                        :password => @password,
+                                                        :entityname => "cssi_carrier"},
+                                                        :content_type => :json))
+      
+      carriers = ""
+      carrier_res.each do |carrier|
+        carriers << carrier << "||"
+      end
+      
+      ap "Carriers: #{carriers}"
+      
+      lob_res = JSON.parse(RestClient.post(@staticentity_url, {:username => @username,
+                                                               :password => @password,
+                                                               :entityname => "cssi_lineofbusiness"},
+                                                               :content_type => :json))
+                                                    
+      lobs = ""
+      lob_res.each do |lob|
+        lobs << lob << "||"
+      end               
+      
+      ap "LOBs: #{lobs}"                               
+      
+      lead_source_res = JSON.parse(RestClient.post(@staticentity_url, {:username => @username,
+                                                                       :password => @password,
+                                                                       :entityname => "cssi_leadsource"},
+                                                                       :content_type => :json))
+                                                            
+      lead_sources = ""
+      lead_source_res.each do |lead_source|
+        lead_sources << lead_source << "||"
+      end             
+      
+      ap "Lead sources: #{lead_sources}"                 
+      
+      ExceptionUtil.context(:result => @result)          
+      
+      @result = { "carriers" => {"names" => carriers},
+                  "line_of_business" => {"names" => lobs},
+                  "lead_source" => {"names" => lead_sources}
+                }
+      
+      ap "@result = #{@result.inspect}"
+    end
+  end
+ 
+  def sync
+    super
+  end
+ 
+  def create(create_hash,blob=nil)
+    
+  end
+ 
+  def update(update_hash)
+    
+  end
+ 
+  def delete(delete_hash)
+    
+  end
+ 
+  def logoff
+    
+  end
+end
---
+++ spec/sources/staticentity_spec.rb
@@ -0,0 +1,25 @@
+require File.join(File.dirname(__FILE__),'..','spec_helper')
+
+describe "Staticentity" do
+  it_should_behave_like "SpecHelper"
+  
+  before(:each) do
+    setup_test_for Staticentity,'testuser'
+  end
+  
+  it "should process Staticentity query" do
+    pending
+  end
+  
+  it "should process Staticentity create" do
+    pending
+  end
+  
+  it "should process Staticentity update" do
+    pending
+  end
+  
+  it "should process Staticentity delete" do
+    pending
+  end
+end
