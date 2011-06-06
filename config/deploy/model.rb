9500fafe5a5b72a62d1b96e947cab4486d30a17f
Brock Spratlen <brockspratlen@gmail.com>
2011-06-06 17:14:21 -0500

    Fully qualifying server names for deployment scripts to support deployments over VPN.

--- config/deploy/model.rb
+++ config/deploy/model.rb
@@ -1,6 +1,6 @@
 # env determines the environment section in settings/settings.yml that will be used by Rhosync
 set :env, :onsite_model
 # target servers 
-role :app, "nrhrho201", "nrhrho202"
+role :app, "nrhrho201.uicnrh.dom", "nrhrho202.uicnrh.dom"
 set :server_name, "https://rhosync.model.insphereis.net"
 set :branch,      "insite_mobile_1_1"
