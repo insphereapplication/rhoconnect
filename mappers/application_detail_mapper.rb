583386923fcbbd205454706a40ca0c91b5a1439d
Vishal Bardoloi <vishal.bardoloi@parivedasolutions.com>
2011-06-14 14:04:51 -0500

    Changed mappings on Application properties

--- mappers/application_detail_mapper.rb
+++ mappers/application_detail_mapper.rb
@@ -3,24 +3,6 @@ class ApplicationDetailMapper < Mapper
 
   def self.map_data_from_client(data)
     
-    if data['lineofbusiness_id'] || data['lineofbusiness_name']
-      data.merge!({
-        "cssi_lineofbusinessid" => {
-          "id" => data['lineofbusiness_id'], 
-          "type"=>"cssi_lineofbusiness"
-        }
-      })
-    end
-
-    if data['carrier_id'] || data['carrier_name']
-      data.merge!({
-        "cssi_carrierid" => {
-          "id" => data['carrier_id'], 
-          "type"=>"cssi_carrier"
-        }
-      })
-    end
-
     data.merge!({
       "cssi_opportunityid" => {
         "id" => data['opportunity_id'], 
@@ -28,29 +10,17 @@ class ApplicationDetailMapper < Mapper
       }
     })
     
-    data.reject!{|k,v| ['opportunity_id','carrier_id','carrier_name','lineofbusiness_id','lineofbusiness_name','temp_id'].include?(k)}
+    data.reject!{|k,v| ['opportunity_id'  ,'temp_id'].include?(k)}
     data  
   end
   
   def map_from_source_hash(app_detail_mapper)
       app_detail_mapper.map! do |value|
 
-        carrier_id = value['cssi_carrierid']
-        unless carrier_id.nil?
-          value.reject!{|k,v| k == 'cssi_carrierid'}
-          value.merge!({'carrier_id' => carrier_id['id'], 'carrier_name' => carrier_id['name']}) unless carrier_id.blank?
-        end
-
         opportunity_id = value['cssi_opportunityid']
         unless carrier_id.nil?
           value.reject!{|k,v| k == 'cssi_opportunityid'}
           value.merge!({'opportunity_id' => opportunity_id['id']}) unless opportunity_id.blank?
-        end
-
-        lineofbusiness_id = value['cssi_lineofbusinessid']
-        unless lineofbusiness_id.nil?
-          value.reject!{|k,v| k == 'cssi_lineofbusinessid'}
-          value.merge!({'lineofbusiness_id' => lineofbusiness_id['id'], 'lineofbusiness_name' => lineofbusiness_id['name']}) unless lineofbusiness_id.blank?
         end
 
         #always filter out attributes that are only set in RhoSync (avoids problems with fixed schema)
