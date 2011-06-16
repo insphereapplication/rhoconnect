bb20e61e0e47d1c0f2112cefabf51d932b867a20
Greg Norz <greg.norz@parivedasolutions.com>
2011-06-16 16:40:52 -0500

    Split upgrade URL in Appinfo info 'force' and 'soft' Commented out the latest version result in AppInfo.query. We're going to roll this out once we have the clients upgraded.

--- sources/app_info.rb
+++ sources/app_info.rb
@@ -4,7 +4,6 @@ class AppInfo < SourceAdapter
   end
  
   def login
-    # TODO: Login to your data source here if necessary
   end
  
   def query(params=nil)
@@ -12,50 +11,49 @@ class AppInfo < SourceAdapter
     ap "*** Settings file = #{settings[:global][:app_info].inspect}"
     mrv = settings[:global][:app_info][:min_required_version]
     lv = settings[:global][:app_info][:latest_version]
-    apple_url = settings[:global][:app_info][:apple_force_upgrade_url]
-    android_url = settings[:global][:app_info][:android_force_upgrade_url]
+    
+    apple_force_upgrade_url = settings[:global][:app_info][:apple_force_upgrade_url]
+    android_force_upgrade_url = settings[:global][:app_info][:android_force_upgrade_url]
+    
+    apple_soft_upgrade_url = settings[:global][:app_info][:apple_soft_upgrade_url]
+    android_soft_upgrade_url = settings[:global][:app_info][:android_soft_upgrade_url]
+    
     ap "*** Minimum required version is #{mrv} ***"
     ap "*** Latest version is #{lv} ***"
-    ap "*** Apple Upgrade URL is #{apple_url} ***"
-    ap "*** Android Upgrade URL is #{android_url} ***"
-    @result = { "1" => { :min_required_version => mrv, :latest_version => lv, :apple_upgrade_url => apple_url, :android_upgrade_url => android_url } }
+    ap "*** Apple Force Upgrade URL is #{apple_force_upgrade_url} ***"
+    ap "*** Android Force Upgrade URL is #{android_force_upgrade_url} ***"
     
-    # TODO: Query your backend data source and assign the records 
-    # to a nested hash structure called @result. For example:
-    # @result = { 
-    #   "1"=>{"name"=>"Acme", "industry"=>"Electronics"},
-    #   "2"=>{"name"=>"Best", "industry"=>"Software"}
-    # }
-    # raise SourceAdapterException.new("Please provide some code to read records from the backend data source")
+    ap "*** Apple Soft Upgrade URL is #{apple_soft_upgrade_url} ***"
+    ap "*** Android Soft Upgrade URL is #{android_soft_upgrade_url} ***"
+    
+    # For backward compatibility, "apple_upgrade_url" and "android_upgrade_url" are used for the forced upgrade URLs
+    @result = { "1" => { :min_required_version => mrv, :apple_upgrade_url => apple_force_upgrade_url, :android_upgrade_url => android_force_upgrade_url } }
+    
+    # Uncomment this (and delete the above line) when we're ready to deploy soft upgrade
+    # @result = { "1" => { :min_required_version => mrv, 
+    #                      :latest_version => lv,
+    #                      :some_new_thing => 'some_new_thing',
+    #                      :another_new_thing => 'another_new_thing',
+    #                      :apple_upgrade_url => apple_force_upgrade_url,
+    #                      :android_upgrade_url => android_force_upgrade_url,
+    #                      :apple_soft_upgrade_url => apple_soft_upgrade_url,
+    #                      :android_soft_upgrade_url => android_soft_upgrade_url } }
+  
   end
  
   def sync
-    # Manipulate @result before it is saved, or save it 
-    # yourself using the Rhosync::Store interface.
-    # By default, super is called below which simply saves @result
     super
   end
  
   def create(create_hash,blob=nil)
-    # TODO: Create a new record in your backend data source
-    # If your rhodes rhom object contains image/binary data 
-    # (has the image_uri attribute), then a blob will be provided
-    # raise "Please provide some code to create a single record in the backend data source using the create_hash"
   end
  
   def update(update_hash)
-    # TODO: Update an existing record in your backend data source
-    # raise "Please provide some code to update a single record in the backend data source using the update_hash"
   end
  
   def delete(delete_hash)
-    # TODO: write some code here if applicable
-    # be sure to have a hash key and value for "object"
-    # for now, we'll say that its OK to not have a delete operation
-    # raise "Please provide some code to delete a single object in the backend application using the object_id"
   end
  
   def logoff
-    # TODO: Logout from the data source if necessary
   end
 end
