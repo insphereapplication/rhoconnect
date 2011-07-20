a4e61630079775066c041fb60dd525a9605ac2f1
Dave Sims <davsims@gmail.com>
2011-07-20 15:56:57 -0500

    added dev config for cap deployment

---
+++ config/deploy/dev.rb
@@ -0,0 +1,7 @@
+# env determines the environment section in settings/settings.yml that will be used by Rhosync
+set :env, :onsite
+# target servers 
+role :app, "nrhrhod401.uicnrh.dom"
+role :resque, "nrhrhod401.uicnrh.dom"
+set :server_name, "https://rhosync.dev.insphereis.net"
+set :branch,      "master"
