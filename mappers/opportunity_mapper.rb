6be25dd34b1f3b85d31f97666cce04f2e207af2b
Patrick Van Deventer <patrick.vandeventer@Patrick-VanDeventer-MacBook-Pro.(none)>
2011-09-16 13:36:37 -0500

    Brock fixed up pats coding errors

--- mappers/opportunity_mapper.rb
+++ mappers/opportunity_mapper.rb
@@ -6,12 +6,10 @@ class OpportunityMapper < Mapper
     data.reject!{|k,v| CLIENT_ONLY_FIELDS.include?(k)}
     data['cssi_fromrhosync'] = 'true'
     
-    new_owner = data['ownerid'].nil?  ? mapper_context['ownerid'] : data['ownerid']
+    current_owner = data['ownerid'].blank?  ? mapper_context[:user_id] : data['ownerid']
     
-    InsiteLogger.info "new_owner = #{new_owner.inspect} mapper_context[ownereid] = #{mapper_context['ownerid']}"
-    
-    if mapper_context['ownerid'] || new_owner
-      data['ownerid'] = {:type => 'systemuser', :id => new_owner }
+    if current_owner
+      data['ownerid'] = {:type => 'systemuser', :id => current_owner }
     end
     
     data  
@@ -23,11 +21,10 @@ class OpportunityMapper < Mapper
       #these fields are not modified from rhodes and should only be injected in map_data_from_client as needed
       
       value.reject!{|k,v|  ['temp_id'].include?(k) }
-      value['ownerid'] = value['ownerid']['id']
-      # for 2.0 Iteration 1 only!
-      # Greg Norz - 2011-06-13 - Leaving this commented in here for now in case we need it for iteration 2. It listed as a merge conflict.
-      #value.reject!{|k,v|  ['opportunityratingcode', 'actualclosedate'].include?(k) }
 
+      # the owner comes across as complex type with id type and name we convert it to id only
+      value['ownerid'] = value['ownerid']['id'] if value['ownerid']
+          
       value
     end
     opportunity_mapper.reduce({}){|sum, value| sum[value["opportunityid"]] = value if value; sum }
