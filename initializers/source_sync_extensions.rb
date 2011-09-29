384dc3bcb710778ae691ebc073868836e42ec780
Brock Spratlen <brockspratlen@gmail.com>
2011-09-29 09:50:00 -0500

    Changed custom push_updates implementation to mirror Rho's fix for doing differential updates to the master document.
    This new logic only changes the specific master document set members that are affected by the update.

--- initializers/source_sync_extensions.rb
+++ initializers/source_sync_extensions.rb
@@ -1,21 +1,28 @@
 module Rhosync
-  class SourceSync
-    def push_object_updates(objects,timeout=10,raise_on_expire=false)
+  class SourceSync    
+    def push_object_updates(data)
       rejected_creates = []
-      @source.lock(:md,timeout,raise_on_expire) do |s|
-        doc = @source.get_data(:md)
-        orig_doc_size = doc.size
-        objects.each do |id,obj|
-          unless doc[id].nil?
-            doc[id].merge!(obj)
-          else
-            rejected_creates.push(id)
+      @source.lock(:md) do |s|
+        doc = s.get_data(:md)
+        Store.db.pipelined do
+          data.each do |key,value|
+            existing_record = doc[key]
+            if existing_record
+              value.each do |attrib,value|
+                existing_value = existing_record[attrib]
+                existing_value = setelement(key,attrib,existing_value) if existing_value 
+                new_value = setelement(key,attrib,value)
+                if existing_value.nil? or existing_value != new_value
+                  Store.db.srem(s.docname(:md), existing_value) if existing_value
+                  Store.db.sadd(s.docname(:md), new_value)
+                end
+              end
+            else
+              rejected_creates.push(key)
+            end
           end
         end
-        diff_count = doc.size - orig_doc_size
-        @source.put_data(:md,doc)
-        @source.update_count(:md_size,diff_count)
-      end
+      end  
       rejected_creates
     end
   end
