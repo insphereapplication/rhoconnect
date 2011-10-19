83363cc8a076e733074af3c3fd01636f4cc483e4
Dwayne Smith <dwayne.smith@healthmarkets.com>
2011-10-19 16:42:15 -0500

    committing lastest production tag to master

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -4,4 +4,4 @@ set :env, :onsite
 role :app, "nrhrho101.pinsp.dom", "nrhrho102.pinsp.dom"
 role :resque, "nrhrho101.pinsp.dom"
 set :server_name, "https://rhosync.insphereis.net"
-set :branch,      "v2.1.4_prod"
+set :branch,      "v3.0.1_prod"
