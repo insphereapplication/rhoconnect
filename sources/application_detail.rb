6dfc53007ddba09ded6bf5a58b2d2115e468f431
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-09-27 14:33:54 -0500

    INS-6921 -  Updated the opportunity temp_id if not correct

--- sources/application_detail.rb
+++ sources/application_detail.rb
@@ -56,6 +56,7 @@ class ApplicationDetail < SourceAdapter
       InsiteLogger.info "CREATE APPLICATION DETAIL"
       ExceptionUtil.context(:current_user => current_user.login, :create_hash => create_hash)
       
+      create_hash = replace_with_guid(create_hash,"opportunity_id","Opportunity")
       result = proxy_create(create_hash)
       
       InsiteLogger.info result
