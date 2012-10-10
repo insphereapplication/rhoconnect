7cbe55f5110532435b4273e13c50ee8ce648cc8f
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-10-10 14:56:10 -0500

    modifying for rhocoonect env

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,7 +1,7 @@
 # env determines the environment section in settings/settings.yml that will be used by Rhoconnect
 set :env, :onsite_dev
 # target servers 
-role :app, "nrhrhod401.uicnrh.dom"
-role :resque, "nrhrhod401.uicnrh.dom"
-set :server_name, "https://rhosync.dev.insphereis.net"
-set :branch,      "master"
+role :app, "nrhrhod404.uicnrh.dom"
+role :resque, "nrhrhod404.uicnrh.dom"
+set :server_name, "http://nrhrhod406"
+set :branch,      "rhoconnect"
