b40dfa183840ede7eb1538a73ef5db3784efdd6a
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-10-11 14:15:40 -0500

    commit tag v3.6.1_prod for production 12.10.2 release

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -4,5 +4,5 @@ set :env, :onsite
 role :app, "nrhrho101.pinsp.dom", "nrhrho102.pinsp.dom"
 role :resque, "nrhrho101.pinsp.dom"
 set :server_name, "https://rhosync.insphereis.net"
-set :branch,      "v3.6.0_prod"
+set :branch,      "v3.6.1_prod"
 
