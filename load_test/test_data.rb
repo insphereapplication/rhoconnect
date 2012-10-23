de94cfde23922fce5affaeb6028ddc96872a63a1
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-10-23 16:31:56 -0500

    Updated load test scripts to Rhoconnect

--- load_test/test_data.rb
+++ load_test/test_data.rb
@@ -11,9 +11,24 @@ module TestData
 
     $lead_types = ['Agent Website','Banner','Classified','Other','Search']
     
+    $status_reasons = ['App Received','Delivery Receipt Outstanding','App Ready To Submit (Sent to Field Office)','Application Needs Info (On Hold)','Submitted to Carrier','App Received By Carrier','Issued','Re-instated','Suspended','Replacement Conversion']
+
+    $status_reasons_pending = ['App Received','Delivery Receipt Outstanding','App Ready To Submit (Sent to Field Office)','Application Needs Info (On Hold)','Submitted to Carrier','App Received By Carrier','Issued','Re-instated','Suspended','Replacement Conversion']
+
+    $status_reasons_active = ['InForce']
+
+    $status_reasons_terminated = ['Withdrawn','Decline','Term After Issue', 'Closed (Incomplete)','Cancelled by Customer']
+    
+    SECONDS_IN_A_DAY = 86400
+    
     def rand_activity_state
        ['Open','Completed'][rand(2)]
      end
+     
+     def rand_previous_create_date
+       minus_days = [3,5,7][rand(3)]
+       (Time.now - (minus_days * SECONDS_IN_A_DAY)).strftime("%Y-%m-%d %H:%M:%S")
+     end    
 
      def rand_array_item(array)
      	array[rand(array.count)]
@@ -58,6 +73,17 @@ module TestData
      def rand_marital_status
        ['Single','Married','Divorced','Widowed'][rand(4)]
      end
+     
+     def rand_status_reason(policy_status)
+       if(policy_status =='Active')
+         rand_array_item($status_reasons_active)
+       elsif(policy_status == 'Terminated')
+         rand_array_item($status_reasons_terminated)
+       else
+         rand_array_item($status_reasons_pending)
+       end
+
+     end
     
     def create_activity_json(type, statecode, parent_contact_id, parent_id, parent_type, statuscode)
       {
@@ -101,26 +127,86 @@ module TestData
       
       res[mock_id] = 
       {
-                  "scheduledstart" => "2011-04-21 17:00:00",
+                  "scheduledstart" => (Time.now + (SECONDS_IN_A_DAY)).strftime("%Y-%m-%d %H:%M:%S"),
                       "object" => mock_id,
                      "phonenumber" => "",
                   "cssi_phonetype" => "",
                      "parent_type" => "Opportunity",
                    "cssi_location" => "Home",
-                         "subject" => "Jaeden, Prohaska - 2011-04-07 11:31:24",
+                         "subject" => "Load Test Create",
                "parent_contact_id" => parent_contact_id,
           "cssi_dispositiondetail" => "",
                             "type" => "PhoneCall",
                        "parent_id" => parent_id,
-                    "scheduledend" => "2011-04-21 18:30:00",
+                    "scheduledend" => (Time.now + (SECONDS_IN_A_DAY + 900)).strftime("%Y-%m-%d %H:%M:%S"),
                      "description" => "",
                 "cssi_disposition" => disposition, #"Left Message"/"Contact Made"/"No Answer"
                        "statecode" => statecode,
                       "statuscode" => statuscode,
+                      "createdon"  => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
                          "temp_id" => mock_id                   
       }
       res
     end  
+    
+    def create_appointment(parent_id, parent_contact_id, statecode = "", statuscode = "")
+      res = {}
+      mock_id = UUIDTools::UUID.random_create.to_s.gsub(/\-/,'')
+      
+      #Codes for scheduled              "statecode" => "Scheduled",   "statuscode" => "Busy"
+      res[mock_id] = 
+      {
+                  "scheduledstart" => (Time.now + SECONDS_IN_A_DAY).strftime("%Y-%m-%d %H:%M:%S"),
+                     "parent_type" => "Opportunity",
+                   "cssi_location" => "Home",
+                         "subject" => "Load Test Create",
+               "parent_contact_id" => parent_contact_id,
+          "cssi_dispositiondetail" => "",
+                            "type" => "Appointment",
+                       "parent_id" => parent_id,
+                    "scheduledend" => (Time.now + (SECONDS_IN_A_DAY + 900)).strftime("%Y-%m-%d %H:%M:%S"),
+                     "description" => "Create appointment",
+                       "statecode" => statecode,
+                      "statuscode" => statuscode,
+                      "createdon"  => (Time.now - SECONDS_IN_A_DAY).strftime("%Y-%m-%d %H:%M:%S"),
+                      "location" => " 1001 Main, coppell, tx 75019",
+                      "cssi_location" => "Ad Hoc",
+                         "temp_id" => mock_id                          
+      
+      }
+      res
+    end
+    
+    def get_fake_policy_data(contact_id, contact, policy_status)
+      fake_data = {}
+      if (policy_status.nil?)
+        status_code = 'Active' 
+      else
+        status_code = policy_status
+      end
+      status_reason = rand_status_reason(policy_status)
+      mock_id = UUIDTools::UUID.random_create.to_s.gsub(/\-/,'')
+       # 'ownerid' => {'type' => 'systemuser', 'id' => identity['id']},
+      fake_data[mock_id] = {
+        'cssi_applicationdate' => "#{rand_year}/#{rand_month}/#{rand_day}",
+        'cssi_paymentmode' => 'Annually',
+        'cssi_policynumber' => Faker::Base.bothify('##???#######').upcase,
+        'cssi_submitteddate' => "#{rand_year}/#{rand_month}/#{rand_day}",
+        'contact_id' => contact_id,
+        'cssi_effectivedate' => "#{rand_year}/#{rand_month}/#{rand_day}",
+        'cssi_applicationnumber' => Faker::Base.numerify('#########'),
+        'statuscode' => status_code,
+        'cssi_primaryinsured' => "#{contact[1]['firstname']} #{contact[1]['lastname']}",
+        'cssi_carrierstatusvalue' => 'Active and paying',
+        'cssi_statusreason' => status_reason,
+        'cssi_insuredtype' => 'Individual',
+        'cssi_annualpremium' => Faker::Base.numerify('####.##'),
+        'cssi_statusdate' => "2012/01/#{rand_day}",
+        'temp_id' => mock_id  
+      };
+
+      fake_data
+    end
     
     def create_left_message_phone_call_data(parent_id, parent_contact_id)
       data = {}
@@ -149,7 +235,8 @@ module TestData
 
        first_name = Faker::Name.first_name
        last_name = Faker::Name.last_name
-       email = "#{first_name}.#{last_name}@loadtest.net"
+       #email = "#{first_name}.#{last_name}@loadtest.net"
+       email = "Patrick.VanDeventer@inspherehq.com"
        #'ownerid' => {'type' => 'systemuser', 'id' => identity['id']},
      	fake_data[1] = {
      		'address1_city' => Faker::Address.city,
@@ -161,7 +248,7 @@ module TestData
      		'firstname' => first_name,
      		'lastname' => last_name,
      		'emailaddress1' => email,
-     		'birthdate' => "#{rand_year}/#{rand_month}/#{rand_day}",
+     		'birthdate' => "#{rand_year}-#{rand_month}-#{rand_day} 00:00:00",
      		'cssi_preferredphone' => preferred_phone,
      		'mobilephone' => Faker::Base.numerify('(###) ###-####'),
      		'telephone1' => Faker::Base.numerify('(###) ###-####'),
@@ -178,7 +265,7 @@ module TestData
      		'cssi_allowcallsmobilephone' => rand_tf,
      		'cssi_spousename' => Faker::Name.first_name,
      		'cssi_spouselastname' => Faker::Name.last_name,
-     		'cssi_spousebirthdate' => "#{rand_year}/#{rand_month}/#{rand_day}",
+     		'cssi_spousebirthdate' => "#{rand_year}-#{rand_month}-#{rand_day} 00:00:00",   
      		'cssi_spouseheightft' => rand_height_feet,
      		'cssi_spouseheightin' => rand_height_inches,
      		'cssi_spouseweight' => rand_weight,
@@ -198,6 +285,29 @@ module TestData
      		'cssi_leadtypeid' => rand_array_item($lead_types),
      		'cssi_inputsource' => 'Integrated',
      		'status_update_timestamp' => Time.now.utc.to_s, 
+     		'createdon' => rand_previous_create_date,
+     		'statecode' => 'Open',
+     		'statuscode' => 'New Opportunity',
+     		'cssi_assetownerid' => 'a6d565bd-f092-e011-b110-0050569c157c',
+        'cssi_lastactivitydate' => Time.now.strftime("%Y-%m-%d %H:%M:%S")
+     	};
+     	fake_data
+     end
+     
+     def get_fake_won_opportunity_data(contact_id, state_code, status_code)
+      #'ownerid' => {'type' => 'systemuser', 'id' => identity['id']}, 
+      fake_data = {}
+     	fake_data[1] = {
+     		'contact_id' => contact_id,
+     		'cssi_leadsourceid' => rand_array_item($lead_sources),
+     		'cssi_leadvendorid' => rand_array_item($lead_vendors),
+     		'cssi_leadtypeid' => rand_array_item($lead_types),
+     		'cssi_inputsource' => 'Integrated',
+     		'status_update_timestamp' => Time.now.utc.to_s, 
+     		'createdon' => Time.now.strftime("%Y-%m-%d %H:%M:%S"),
+     		'statecode' => state_code,
+     		'statuscode' => status_code,
+     		'cssi_assetownerid' => 'a6d565bd-f092-e011-b110-0050569c157c',
         'cssi_lastactivitydate' => Time.now.strftime("%Y-%m-%d %H:%M:%S")
      	};
      	fake_data
