3ca3615130b5ba793836cd450506f30f046ceade
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-11-09 09:46:05 -0600

    Correcting hostnames

--- config/deploy/model.rb
+++ config/deploy/model.rb
@@ -1,7 +1,7 @@
 # env determines the environment section in settings/settings.yml that will be used by Rhoconnect
 set :env, :onsite_model
 # target servers 
-role :app, "nrhrho204.uicnrh.dom", "nrhrho205.uicnrh.dom"
-role :resque, "nrhrho204.uicnrh.dom"
-set :server_name, "https://rhoconect.model.insphereis.net"
+role :app, "nrhrhom204.uicnrh.dom", "nrhrhom205.uicnrh.dom"
+role :resque, "nrhrhom204.uicnrh.dom"
+set :server_name, "https://rhoconnect.model.insphereis.net"
 set :branch,      "master"
