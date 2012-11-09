c03b719ee59536184a88f76aa8ef72cd90df1258
Dwayne Smith <dwayne.smith@healthmarkets.com>
2012-11-09 09:41:13 -0600

    Updating to rhoconnect model

--- config/deploy/model.rb
+++ config/deploy/model.rb
@@ -1,7 +1,7 @@
 # env determines the environment section in settings/settings.yml that will be used by Rhoconnect
 set :env, :onsite_model
 # target servers 
-role :app, "nrhrho201.uicnrh.dom", "nrhrho202.uicnrh.dom"
-role :resque, "nrhrho201.uicnrh.dom"
-set :server_name, "https://rhosync.model.insphereis.net"
+role :app, "nrhrho204.uicnrh.dom", "nrhrho205.uicnrh.dom"
+role :resque, "nrhrho204.uicnrh.dom"
+set :server_name, "https://rhoconect.model.insphereis.net"
 set :branch,      "master"
