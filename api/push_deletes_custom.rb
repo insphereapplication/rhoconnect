3c976a1ae4b679af82a5971902a55793ecf8d476
Dwayne Smith <dwayne.smith@healthmarkets.com>
2013-01-17 11:01:54 -0600

    INS-7421 - Additional logging and fix around push deletes

--- api/push_deletes_custom.rb
+++ api/push_deletes_custom.rb
@@ -3,7 +3,8 @@
     deleted_objects = params[:objects]
     InsiteLogger.info(:format_and_join => ["PUSH DELETES #{params[:source_id]} FOR #{params[:user_id]}: ",deleted_objects])
     deleted_objects = RedisUtil.get_existing_keys("#{params[:source_id]}", "#{params[:user_id]}", deleted_objects)
-    if deleted_objects
+    InsiteLogger.info(:format_and_join => ["PUSH DELETES #{params[:source_id]} AFTER KEY CHECK FOR #{params[:user_id]}: ",deleted_objects])
+    if !deleted_objects.empty?
       source = Source.load(params[:source_id],{:app_id=>APP_NAME,:user_id=>params[:user_id]})
       source_sync = SourceSync.new(source)
       source_sync.push_deletes(deleted_objects,nil,nil,false)
