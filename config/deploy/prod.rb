4fd9b57e75ed56a9b0eccbb8bbe6b4e0735f4429
admin <admin@admins-Mac-mini.local>
2016-09-15 16:17:14 -0500

    Updating product version to v5.4.02_prod

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -4,6 +4,6 @@ set :env, :onsite
 role :app, "nrhrhop104.uicnrh.dom", "nrhrhop105.uicnrh.dom"
 role :resque, "nrhrhop104.uicnrh.dom"
 set :server_name, "https://rhoconnect.insphereis.net"
-set :branch,      "v5.4.01_prod"
+set :branch,      "v5.4.02_prod"
 
 
